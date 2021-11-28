2b090d9ca624165d3b15c413a41ef5cfb113c497
Petro Silenius <petro.silenius@gmail.com>
2021-11-28 17:47:07 +0200

    Change links and text content to match  author

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,10 +1,7 @@
 module.exports = {
   // keep it short! shown in the header
-  title: 'Frontend Masters: Serverless with Jason Lengstorf',
+  title: 'Frontend Masters: Introduction to Serverless Functions',
 
   // these are all optional and add links to the footer
-  repo: 'jlengstorf/frontendmasters-serverless',
-  // episode:
-  //   'https://www.learnwithjason.dev/creating-css-variable-font-text-effects',
-  // tutorial: 'https://codepen.io/jlengstorf/pen/QWbdLjb',
+  repo: 'PetroSilenius/serverless-intro',
 };
